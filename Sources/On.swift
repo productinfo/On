904af96d4cc49115767936a3e36586ab2f4d754f
Khoa Pham <onmyway133@gmail.com>
2017-05-05 00:24:52 +0200

    Add KeyPathTarget

--- Sources/On.swift
+++ Sources/On.swift
@@ -4,3 +4,4 @@ public typealias Action = () -> Void
 public typealias StringAction = (String) -> Void
 public typealias FloatAction = (Float) -> Void
 public typealias DateAction = (Date) -> Void
+public typealias AnyAction = (Any) -> Void
