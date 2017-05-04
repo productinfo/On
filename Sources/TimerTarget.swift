a2c3597857b6042cd8bea2bce736fe8916f301ee
Khoa Pham <onmyway133@gmail.com>
2017-05-04 23:22:29 +0200

    Add TimerTarget

--- Sources/TimerTarget.swift
+++ Sources/TimerTarget.swift
@@ -1,9 +1,50 @@
-//
-//  TimerTarget.swift
-//  On
-//
-//  Created by Khoa Pham on 04/05/2017.
-//  Copyright © 2017 Khoa Pham. All rights reserved.
-//
-
 import Foundation
+
+public class TimerTarget: NSObject {
+  var fireBlock: Action?
+
+  // MARK: - Action
+
+  func didFire() {
+    fireBlock?()
+  }
+
+  // MARK: - Public
+
+  func fire(_ action: @escaping Action) {
+    self.fireBlock = action
+  }
+}
+
+public extension Timer {
+  static func scheduledTimer(withTimeInterval interval: TimeInterval, repeats: Bool) -> Timer {
+    let on = TimerTarget()
+    let timer = Timer.scheduledTimer(timeInterval: interval,
+                                     target: on,
+                                     selector: #selector(TimerTarget.didFire),
+                                     userInfo: nil,
+                                     repeats: repeats)
+    timer.on = on
+    return timer
+  }
+}
+
+extension Timer {
+  private struct AssociatedKey {
+    static var on = "on"
+  }
+
+  var on: TimerTarget {
+    get {
+      guard let value = objc_getAssociatedObject(self, &AssociatedKey.on) as? TimerTarget else {
+        fatalError("Must use scheduledTimer(withTimeInterval interval: TimeInterval, repeats: Bool)")
+      }
+
+      return value
+    }
+
+    set {
+      objc_setAssociatedObject(self, &AssociatedKey.on, newValue as TimerTarget?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+}
