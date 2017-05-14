e7d320c016c3d7681298a462366e948af83984a7
Khoa Pham <onmyway133@gmail.com>
2017-05-14 17:56:34 +0200

    Return associated object

--- Sources/Core/OnAware.swift
+++ Sources/Core/OnAware.swift
@@ -1,18 +1,25 @@
 import Foundation
 
+private struct AssociatedKey {
+  static var key = "on"
+}
+
 public protocol OnAware {
   associatedtype OnAwareHostType
 
-  var on: Container<OnAwareHostType> { get set }
+  var on: Container<OnAwareHostType> { get }
 }
 
 extension OnAware {
   public var on: Container<Self> {
     get {
-      return Container(host: self)
-    }
-    set {
+      if let value = objc_getAssociatedObject(self, &AssociatedKey.key) as? Container<Self> {
+        return value
+      }
 
+      let value = Container(host: self)
+      objc_setAssociatedObject(self, &AssociatedKey.key, value, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+      return value
     }
   }
 }
