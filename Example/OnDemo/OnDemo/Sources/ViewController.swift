40b8468e3d9dc4c022d505f431af642596c3fe5b
Khoa Pham <onmyway133@gmail.com>
2017-05-14 18:19:19 +0200

    Add UIs

--- Example/OnDemo/OnDemo/Sources/ViewController.swift
+++ Example/OnDemo/OnDemo/Sources/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import On
+import Anchors
 
 class ViewController: UIViewController {
 
@@ -7,7 +8,41 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     view.backgroundColor = UIColor.white
 
-    let button 
+    button()
+    textField()
+  }
+
+  func button() {
+    let button = UIButton()
+    button.setTitle("Click", for: .normal)
+    button.setTitleColor(UIColor.black, for: .normal)
+    view.addSubview(button)
+
+    activate(button.anchor.center)
+
+    button.on.tap({
+      print("tap")
+    })
+  }
+
+  func textField() {
+    let textField = UITextField()
+    view.addSubview(textField)
+
+    activate(
+      textField.anchor.width.constant(100),
+      textField.anchor.height.constant(40),
+      textField.anchor.centerX,
+      textField.anchor.centerY.constant(20)
+    )
+
+    textField.on.text({
+      print($0)
+    })
+
+    textField.on.keyPath(#keyPath(UITextField.text), {
+      print($0)
+    })
   }
 }
 
