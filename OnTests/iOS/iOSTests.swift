e8d0e2933e5d7fbd2c6a02e400872d9d9ee1981d
Khoa Pham <onmyway133@gmail.com>
2017-05-15 22:55:48 +0200

    Add tests

--- OnTests/iOS/iOSTests.swift
+++ OnTests/iOS/iOSTests.swift
@@ -1,27 +1,33 @@
 import XCTest
+import On
 
-class iOSTests: XCTestCase {
+class OnTests: XCTestCase {
 
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
+  func testButton() {
+    let button = UIButton()
 
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
+    var called = false
+    button.on.tap {
+      called = true
     }
+
+    button.sendActions(for: .touchUpInside)
+
+    wait(for: 0.5)
+    XCTAssertTrue(called)
   }
 
+  func testKeyPath() {
+    let label = UILabel()
+
+    var text: String?
+    label.on.observe(keyPath: #keyPath(UILabel.text), {
+      text = $0 as? String
+    })
+
+    label.text = "hello"
+
+    wait(for: 0.1)
+    XCTAssertEqual(text, "hello")
+  }
 }
