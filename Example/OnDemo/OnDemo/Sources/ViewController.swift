c66b5fc0149b8aa7c47cd55099e3c515a178e741
Khoa Pham <onmyway133@gmail.com>
2017-05-20 15:56:33 +0200

    Add good cheap fast

--- Example/OnDemo/OnDemo/Sources/ViewController.swift
+++ Example/OnDemo/OnDemo/Sources/ViewController.swift
@@ -3,7 +3,33 @@ import On
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var goodSwitch: UISwitch!
+  @IBOutlet weak var cheapSwitch: UISwitch!
+  @IBOutlet weak var fastSwitch: UISwitch!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    let good = self.goodSwitch!
+    let cheap = self.cheapSwitch!
+    let fast = self.fastSwitch!
+
+    good.on.change {
+      if [good, cheap, fast].filter({ $0.isOn }).count == 3 {
+        fast.setOn(false, animated: true)
+      }
+    }
+
+    cheap.on.change {
+      if [good, cheap, fast].filter({ $0.isOn }).count == 3 {
+        good.setOn(false, animated: true)
+      }
+    }
+
+    fast.on.change {
+      if [good, cheap, fast].filter({ $0.isOn }).count == 3 {
+        cheap.setOn(false, animated: true)
+      }
+    }
   }
 }
