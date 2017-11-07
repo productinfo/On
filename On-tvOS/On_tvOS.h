79482cc8fe1f2f8bd8e72466bd8fef1d7a2e0a07
Khoa Pham <onmyway133@gmail.com>
2017-11-07 11:38:23 +0100

    Remove travis

---
+++ On-tvOS/On_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  On_tvOS.h
+//  On-tvOS
+//
+//  Created by Khoa Pham on 07.11.2017.
+//  Copyright © 2017 Khoa Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for On_tvOS.
+FOUNDATION_EXPORT double On_tvOSVersionNumber;
+
+//! Project version string for On_tvOS.
+FOUNDATION_EXPORT const unsigned char On_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <On_tvOS/PublicHeader.h>
+
+
